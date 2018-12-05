9b15b51b593704ebd2c1cc860c184b57cffaf641
dennis.shim <dennis.shim@kakaocorp.com>
2018-12-05 20:37:47 +0900

    Add web flux style request mapping handler

--- src/main/java/io/github/nvidian7/application/web/IndexController.java
+++ src/main/java/io/github/nvidian7/application/web/IndexController.java
@@ -2,7 +2,9 @@ package io.github.nvidian7.application.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @Slf4j
@@ -11,6 +13,11 @@ public class IndexController {
     @GetMapping("/")
     public Object healthCheck() {
         return "Hello, world!";
+    }
+
+    @GetMapping("/{id}")
+    private Mono<String> exampleResource(@PathVariable String id) {
+        return Mono.just("Hello " + id);
     }
 
 }

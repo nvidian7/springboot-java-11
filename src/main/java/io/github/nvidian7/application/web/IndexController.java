4d4147503b0dfcbc5b9f93fd16c9dd974ac1447a
nvidian7 <woojun.shim@gmail.com>
2019-01-19 17:26:36 +0900

    Update spring boot version

--- src/main/java/io/github/nvidian7/application/web/IndexController.java
+++ src/main/java/io/github/nvidian7/application/web/IndexController.java
@@ -5,12 +5,15 @@ import io.github.nvidian7.application.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class IndexController {
+
+    private final static String CURRENT_TIME_API_URL = "http://worldclockapi.com/api/json/utc/now";
 
     private final AccountRepository accountRepository;
 
@@ -29,6 +32,11 @@ public class IndexController {
         Account account = Account.builder().id(id).name(name).build();
         account = accountRepository.save(account);
         return Mono.just(account);
+    }
+
+    @GetMapping("/now")
+    private Mono<Object> now() {
+        return WebClient.create(CURRENT_TIME_API_URL).get().retrieve().bodyToMono(Object.class);
     }
 
 }
